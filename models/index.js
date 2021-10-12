21605a6700ef3e0cb4610ca1255205943723db32
Erick Fernando Guzmán Valerio <guzmi.dev@gmail.com>
2021-10-12 13:43:26 -0600

    :art: Categorias y Productos

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const Categoria = require("./categoria");
 const Rol = require("./Role");
 const Server = require("./server");
-const Usuaurio = require("./usuario");
+const Usuario = require("./usuario");
 const Producto = require("./producto");
 
 module.exports = {
   Categoria,
   Rol,
   Server,
-  Usuaurio,
+  Usuario,
   Producto,
 };
